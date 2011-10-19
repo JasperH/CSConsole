9d4a92f23f8c644f5f143f255dc048690259cf8d
MHGameWorkBase <mhgamework@gmail.com>
2011-10-19 14:46:52 +0200

    Added some ignore + testclass
    Signed-off-by: MHGameWorkBase <mhgamework@gmail.com>

---
+++ console2/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace console2
+{
+    public class TestClass
+    {
+        public void EchoMe(string args)
+        {
+            
+            Console.WriteLine(args);
+        }
+    }
+}
